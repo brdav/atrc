76d805514aea4fde2be5765edb6ce02091abafe6
brdav <brdavid@vision.ee.ethz.ch>
2023-08-09 11:29:10 +0200

    fix format

--- src/data/nyud.py
+++ src/data/nyud.py
@@ -32,20 +32,12 @@ def download_file_from_google_drive(id, destination):
 
     session = requests.Session()
 
-    # response = session.get(URL, params = { 'id' : id }, stream = True)
-
-    # token = None
-    # for key, value in response.cookies.items():
-    #     if key.startswith('download_warning'):
-    #         token = value
-
-    # if token:
-    params = { 'id' : id, 'confirm' : 1 }
-    response = session.get(URL, params = params, stream = True)
+    params = {'id': id, 'confirm': 1}
+    response = session.get(URL, params=params, stream=True)
 
     with open(destination, "wb") as f:
         for chunk in response.iter_content(CHUNK_SIZE):
-            if chunk: # filter out keep-alive new chunks
+            if chunk:  # filter out keep-alive new chunks
                 f.write(chunk)
 
 
